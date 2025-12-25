75bfbf3c54090bbb9fb639122b29033876dbd261
Lucas Garron <code@garron.net>
2025-12-25 00:09:06 -0800

    Overhaul.

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
+.PHONY: check
+check: lint check-package.json
+
 .PHONY: lint
 lint: setup
 	bun x @biomejs/biome check
+	bun x -- bun-dx --package typescript tsc -- --noEmit --project ./tsconfig.json
 
 .PHONY: format
 format: setup
@@ -10,10 +14,21 @@ format: setup
 setup:
 	bun install --frozen-lockup
 
-.PHONY: reset
-reset:
-	rm -rf ./node_modules
+.PHONY: check-package.json
+check-package.json:
+	bun x -- bun-dx --package @cubing/dev-config package.json -- check
 
 .PHONY: publish
 publish:
 	npm publish
+
+.PHONY: prepublishOnly
+prepublishOnly: clean check
+
+.PHONY: clean
+clean:
+
+.PHONY: reset
+reset: clean
+	rm -rf ./node_modules
+
---
+++ src/lib/parseArgs.ts
@@ -0,0 +1,53 @@
+import { argv } from "node:process";
+import {
+  argument,
+  choice,
+  map,
+  message,
+  object,
+  option,
+  optional,
+} from "@optique/core";
+import { path, run } from "@optique/run";
+import { Path } from "path-class";
+import { version as VERSION } from "../../package.json";
+
+export function parseArgs() {
+  return run(
+    object({
+      dryRun: option("--dry-run"),
+      mkdirDestinationRootIfMissing: optional(
+        map(
+          option(
+            "-mkdir-destination-root-if-missing",
+            choice(["true", "false"]),
+          ),
+          Boolean,
+        ),
+      ),
+      sourceDir: map(
+        argument(
+          path({ mustExist: true, type: "directory", metavar: "SOURCE_DIR" }),
+        ),
+        Path.fromString,
+      ),
+      destinationDir: map(
+        argument(path({ metavar: "DESTINATION_DIR" })),
+        Path.fromString,
+      ),
+    }),
+    {
+      programName: new Path(argv[1]).basename.path,
+      description: message`The commandline tool of the future!`,
+      help: "option",
+      completion: {
+        mode: "option",
+        name: "plural",
+      },
+      version: {
+        mode: "option",
+        value: VERSION,
+      },
+    },
+  );
+}
